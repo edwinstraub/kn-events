ddd42cc968faa73a249a3ddb85436f2994a7b768
Edwin Straub <edwin.straub@adesso-romania.ro>
2024-03-27 10:44:18 +0200

    First commit

---
+++ src/test/java/ais/de/funqy/cloudevent/FunqyIT.java
@@ -0,0 +1,10 @@
+package ais.de.funqy.cloudevent;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class FunqyIT extends FunqyTest {
+
+    // Run the same tests
+
+}
